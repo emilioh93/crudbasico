c4088793754e49cb647cbf27b21fa94eace82789
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-04-16 12:13:07 -0300

    se agregó lógica de formulario

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Form } from 'react-bootstrap';
 
 const AgregarProducto = () => {
+
+    const [nombreProducto, setNombreProducto] = useState('');
+    const [precioProducto, setPrecioProducto] = useState(0);
+    const [categoria, setCategoria] = useState('');
+
+    const leerCategoria = (e)=> {
+        setCategoria(e.target.value);
+    }
+
     return (
         <Container>
             <Form className="my-5">
                 <h1 className="my-5">Agregar nuevo producto</h1>
                 <Form.Group>
                     <Form.Label>Nombre del producto*</Form.Label>
-                    <Form.Control type="text" placeholder="Café"></Form.Control>
+                    <Form.Control type="text" placeholder="Café" onChange={(e)=> setNombreProducto(e.target.value)}></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Precio*</Form.Label>
-                    <Form.Control type="number" placeholder="50"></Form.Control>
+                    <Form.Control type="number" placeholder="50" onChange={(e)=> setPrecioProducto(e.target.value)}></Form.Control>
                 </Form.Group>
                 <h3 className="text-center mt-4">Categoría</h3>
                 <div className="text-center my-4">
-                    <Form.Check inline type="radio" name="categoria" label="Bebida caliente"></Form.Check>
-                    <Form.Check inline type="radio" name="categoria" label="Bebida fría"></Form.Check>
-                    <Form.Check inline type="radio" name="categoria" label="Dulce"></Form.Check>
-                    <Form.Check inline type="radio" name="categoria" label="Salado"></Form.Check>
+                    <Form.Check inline type="radio" name="categoria" label="Bebida caliente" value="bebida-caliente" onChange={leerCategoria}></Form.Check>
+                    <Form.Check inline type="radio" name="categoria" label="Bebida fría" value="bebida-fria" onChange={leerCategoria}></Form.Check>
+                    <Form.Check inline type="radio" name="categoria" label="Dulce" value="dulce" onChange={leerCategoria}></Form.Check>
+                    <Form.Check inline type="radio" name="categoria" label="Salado" value="salado" onChange={leerCategoria}></Form.Check>
                 </div>
             </Form>
         </Container>
