3d68d691632c3617871accda61a431645c947bd4
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-04-19 17:27:57 -0300

    se agregó la validación y el componmente de error

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
-import { Container, Form } from 'react-bootstrap';
+import { Container, Form, Button, Alert } from 'react-bootstrap';
 
 const AgregarProducto = () => {
 
     const [nombreProducto, setNombreProducto] = useState('');
     const [precioProducto, setPrecioProducto] = useState(0);
     const [categoria, setCategoria] = useState('');
+    const [error, setError] = useState(false);
 
     const leerCategoria = (e)=> {
         setCategoria(e.target.value);
     }
 
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        // Validaciones
+        if(nombreProducto.trim() !== "" && precioProducto.trim() !== "" && categoria !== ""){
+            // Si está todo ok, envío los datos del producto a la API
+            setError(false);
+        } else {
+            // Si no está todo ok, valido el error
+            setError(true);
+        }
+    }
+
     return (
         <Container>
-            <Form className="my-5">
+            <Form className="my-5" onSubmit={handleSubmit}>
                 <h1 className="my-5">Agregar nuevo producto</h1>
                 <Form.Group>
                     <Form.Label>Nombre del producto*</Form.Label>
@@ -30,6 +43,10 @@ const AgregarProducto = () => {
                     <Form.Check inline type="radio" name="categoria" label="Dulce" value="dulce" onChange={leerCategoria}></Form.Check>
                     <Form.Check inline type="radio" name="categoria" label="Salado" value="salado" onChange={leerCategoria}></Form.Check>
                 </div>
+                <Button variant="danger" type="submit" className="w-100">Guardar</Button>
+                {
+                    (error === true) ? (<Alert variant="danger" className="my-5">Faltan cargar datos obligatorios</Alert>) : null
+                }
             </Form>
         </Container>
     );
